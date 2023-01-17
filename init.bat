a98c433c941d9b0d025be49bf0744eaf0b866a73
YasminHamid <yasmin.hamid.eu@gmail.com>
2023-01-17 21:25:54 +0200

    implementation

--- init.bat
+++
@@ -1,9 +0,0 @@
-curl -X DELETE "localhost:9200/client"
-curl -X PUT "localhost:9200/client"
-curl -X PUT "localhost:9200/client/_mapping" -H "Content-Type: application/json" -d @clientmapping.json
-curl -s -H "Content-Type: application/json" -X POST "localhost:9200/client/_bulk" --data-binary @client.json
-
-curl -X DELETE "localhost:9200/leads"
-curl -X PUT "localhost:9200/leads"
-curl -X PUT "localhost:9200/leads/_mapping" -H "Content-Type: application/json" -d @leadsmapping.json
-curl -s -H "Content-Type: application/json" -X POST "localhost:9200/leads/_bulk" --data-binary @leads.json
